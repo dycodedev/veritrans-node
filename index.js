1dce397189614506eb86a2c219a8e80b5f490817
Alwin Arrasyid <alwin.ridd@gmail.com>
2015-10-30 15:59:45 +0700

    Added `options` checker

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 'use strict';
 
-function Veritrans() {
+const utils = require('./lib/utils');
+
+function Veritrans(options) {
+    if (!utils.hasProperty(options, ['url', 'serverKey', 'serverPass'])) {
+        throw new Error('options must contains url, serverKey and serverPass property!');
+    }
+
+    this.options = options;
 }
 
 module.exports = Veritrans;
