cb68f5a41faab27abefb6c4830112c4e1fa595f0
Alwin Arrasyid <alwin.ridd@gmail.com>
2015-11-03 17:45:12 +0700

    Added toBase64 function and its test

--- test/test_utils.js
+++ test/test_utils.js
@@ -50,4 +50,25 @@ describe('lib/utils.js', () => {
         });
 
     });
+
+    describe('.toBase64(source)', () => {
+        const base64RegEx = /^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$/;
+
+        it('should return false if it is passed invalid argument type', () => {
+            expect(utils.toBase64(1)).to.equal(false);
+            expect(utils.toBase64(.5)).to.equal(false);
+            expect(utils.toBase64({})).to.equal(false);
+            expect(utils.toBase64([])).to.equal(false);
+
+            expect(utils.toBase64(new Buffer('a'))).to.be.a('string');
+            expect(utils.toBase64('a')).to.be.a('string');
+        });
+
+        it('should return valid base64 string', () => {
+            // see: http://stackoverflow.com/questions/475074/regex-to-parse-or-validate-base64-data/475217#475217
+
+            expect(base64RegEx.test(utils.toBase64(new Buffer('a')))).to.equal(true);
+            expect(base64RegEx.test(utils.toBase64('a'))).to.equal(true);
+        });
+    });
 });
