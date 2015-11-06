d3691fa8d46f6f66c61ffc45b2b33cad1a96ab5e
Alwin Arrasyid <alwin.ridd@gmail.com>
2015-11-06 18:26:33 +0700

    Untested

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
 'use strict';
 
+const _ = require('lodash');
+
 const utils = require('./lib/utils');
+const http = require('./lib/http');
 
 function Veritrans(options) {
-    if (!utils.hasProperty(options, ['url', 'serverKey', 'serverPass'])) {
+    if (!utils.hasProperty(options, ['url', 'merchantId', 'clientKey', 'serverKey'])) {
         throw new Error('options must contains url, serverKey and serverPass property!');
     }
 
-    this.options = options;
+    this.baseURL = options.URL;
+    this.credentials = _.pick(options, ['serverKey', 'serverPass']);
 }
+
+Veritrans.prototype.credentials = {
+    setServerPass(serverPass) {
+        this.credentials.serverPass = serverPass;
+    },
+
+    getServerPass() {
+        return this.credentials.serverPass;
+    },
+
+    setServerKey
+};
+
+Veritrans.prototype.transaction = {
+
+    status(orderId, callback) {
+        http.get(`${this.baseURL}/${orderId}/status`, {}, this.prototype.credentials, (err, response) => {
+            if (err) {
+                return callback(err);
+            }
+
+            const body = response.body;
+
+            if (body.status_code >= 400) {
+                const error = new Error(body.status_message);
+                error.statusCode = body.status_code;
+
+                return callback(error);
+            }
+
+            return callback(null, body);
+        });
+    },
+};
 
 module.exports = Veritrans;
---
+++ test/test_veritrans.js
@@ -0,0 +1,5 @@
+'use strict';
+
+// specify your own config file...
+const config = require('./config.js');
+const Veritrans = require('../');
