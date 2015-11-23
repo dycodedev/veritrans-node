8490c566fafd49e09cd4d88eef632737cf6a2dcd
Alwin Arrasyid <alwin.ridd@gmail.com>
2015-11-23 13:19:48 +0700

    Checking status is fully tested

---
+++ test/test_transaction.js
@@ -0,0 +1,37 @@
+'use strict';
+
+// NOTE: specify your own config file...
+// It must contains merchantId, clientKey, serverKey, and url.
+const expect = require('chai').expect;
+const Veritrans = require('../');
+const utils = require('../lib/utils');
+const config = require('../config.json');
+
+const vt = new Veritrans(config);
+
+// Change the values with your transaction ids,
+const validTransactIds = [
+    'Sample Order-1',
+]
+
+describe('lib/vt/transaction.js', () => {
+
+    describe('status(id, callback)', () => {
+        it('Should carry error if using invalid order id', done => {
+            vt.transaction.status('notavalidId', (err, body) => {
+                expect(err).not.to.be.null;
+                expect(body).to.be.undefined;
+                return done();
+            });
+        });
+
+        it('Should carry results if using valid id', done => {
+            vt.transaction.status(validTransactIds[0], (err, body) => {
+                expect(err).to.be.nulll
+                expect(body).not.to.be.empty;
+
+                return done();
+            });
+        });
+    });
+});
