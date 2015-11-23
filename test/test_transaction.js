d5c6cd796fcce974a2d939d66573c06671a82fa5
Alwin Arrasyid <alwin.ridd@gmail.com>
2015-11-23 18:18:09 +0700

    Added cancel, capture, approve, expire transaction with their test cases

--- test/test_transaction.js
+++ test/test_transaction.js
@@ -91,4 +91,79 @@ describe('lib/vt/transaction.js', function() {
             });
         });
     });
+
+    describe('capture(paylad, callback)', () => {
+        it('Should carry error if payload is invalid', done => {
+            vt.transaction.capture('notavalidId', (err, body) => {
+                expect(err).not.to.be.null;
+                expect(body).to.be.undefined;
+
+                return done();
+            });
+        });
+
+        it('Should carry error if payload contains invalid id', done => {
+            const payload = {
+                transaction_id: 'notavalidId'
+            };
+
+            vt.transaction.capture(payload, (err, body) => {
+                expect(err).not.to.be.null;
+                expect(body).to.be.undefined;
+
+                return done();
+            });
+        });
+
+        it('Should carry result if using valid id', done => {
+            const payload = {
+                transaction_id: validTransactIds[0],
+            };
+
+            vt.transaction.capture(payload, (err, body) => {
+                expect(err).not.to.be.null;
+                expect(body).to.be.undefined;
+
+                return done();
+            });
+        });
+    });
+
+    describe('cancel(id, callback)', () => {
+        it('Should carry error if using invalid id', done => {
+            vt.transaction.cancel('notavalidId', (err, body) => {
+                expect(err).not.to.be.null;
+                expect(body).to.be.undefined;
+
+                return done();
+            });
+        });
+
+        it('Should carry result if using valid id', done => {
+            vt.transaction.cancel(validTransactIds[0], (err, body) => {
+                expect(err).to.be.null;
+                expect(body).not.to.be.undefined;
+                expect(body).to.be.an('object');
+            });
+        });
+    });
+
+    describe('expire(id, callback)', () => {
+        it('Should carry error if using invalid id', done => {
+            vt.transaction.expire('notavalidId', (err, body) => {
+                expect(err).not.to.be.null;
+                expect(body).to.be.undefined;
+
+                return done();
+            });
+        });
+
+        it('Should carry result if using valid id', done => {
+            vt.transaction.expire(validTransactIds[0], (err, body) => {
+                expect(err).to.be.null;
+                expect(body).not.to.be.undefined;
+                expect(body).to.be.an('object');
+            });
+        });
+    });
 });
