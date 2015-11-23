fe1410c0e445498303c24e93baae1a1a2df83ef4
Alwin Arrasyid <alwin.ridd@gmail.com>
2015-11-23 13:15:52 +0700

    Moved credential getter & setter

--- index.js
+++ index.js
@@ -1,43 +1,17 @@
 'use strict';
 
 const _ = require('lodash');
+const utils = require('./lib/utils');
 
 function Veritrans(options) {
     if (!utils.hasProperty(options, ['url', 'merchantId', 'clientKey', 'serverKey'])) {
         throw new Error('options must contains url, serverKey and serverKey property!');
     }
 
-    this.baseURL = options.URL;
+    this.baseUrl = options.url;
     this.credentials = _.pick(options, ['serverKey', 'clientKey', 'merchantId']);
+    this.transaction = require('./lib/vt/transaction')(this);
+    this.credential = require('./lib/credential')(this);
 }
-
-Veritrans.prototype.credential = {
-    setServerKey(serverKey) {
-        this.credentials.serverKey = serverKey;
-    },
-
-    getServerKey() {
-        return this.credentials.serverKey;
-    },
-
-    setClientKey(clientKey) {
-        this.credentials.clientKey = serverKey;
-    },
-
-    getClientKey() {
-        return this.credentials.clientKey;
-    },
-
-    setMerchantId(id) {
-        this.credentials.merchantId = id;
-    },
-
-    getMerchantId() {
-        return this.credentials.merchantId;
-    }
-};
-
-Veritrans.prototype.transaction = {
-};
 
 module.exports = Veritrans;
--- test/test_veritrans.js
+++ test/test_veritrans.js
@@ -1,5 +1,34 @@
 'use strict';
 
-// specify your own config file...
-const config = require('./config.js');
+// NOTE: specify your own config file...
+// It must contains merchantId, clientKey, serverKey, and url.
+const expect = require('chai').expect;
 const Veritrans = require('../');
+const config = require('../config.json');
+
+const vt = new Veritrans(config);
+
+describe('index.js', () => {
+    describe('Veritrans', () => {
+        it('Should have transaction properties', () => {
+            expect(vt.transaction).to.be.an('object');
+            expect(vt.transaction).not.to.be.empty;
+        });
+
+        it('Should have credential properties', () => {
+            expect(vt.credential).to.be.an('object');
+            expect(vt.credential).not.to.be.empty;
+        });
+
+        it('Should property change credential properties', () => {
+            vt.credential.setServerKey('Aha');
+            expect(vt.credential.getServerKey()).to.equal('Aha');
+
+            vt.credential.setClientKey('Aha');
+            expect(vt.credential.getClientKey()).to.equal('Aha');
+
+            vt.credential.setMerchantId('Id');
+            expect(vt.credential.getMerchantId()).to.equal('Id');
+        });
+    });
+});
