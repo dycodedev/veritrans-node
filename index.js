c757a6d708bdfa6fdbf879feed2130b4cb91074f
Alwin Arrasyid <alwin.ridd@gmail.com>
2015-11-26 17:57:22 +0700

    No longer requires merchant id

--- index.js
+++ index.js
@@ -3,15 +3,14 @@
 const _ = require('lodash');
 const utils = require('./lib/utils');
 
-function Veritrans(options) {
-    if (!utils.hasProperty(options, ['url', 'merchantId', 'clientKey', 'serverKey'])) {
+function Veritrans(serverKey) {
+    if (!utils.hasProperty(options, ['url', 'clientKey', 'serverKey'])) {
         throw new Error('options must contains url, serverKey and serverKey property!');
     }
 
     this.baseUrl = options.url;
-    this.credentials = _.pick(options, ['serverKey', 'clientKey', 'merchantId']);
+    this.credentials = _.pick(options, ['serverKey', 'clientKey']);
     this.transaction = require('./lib/vt/transaction')(this);
-    this.credential = require('./lib/credential')(this);
     this.creditcard = require('./lib/vt/creditcard')(this);
 }
 
